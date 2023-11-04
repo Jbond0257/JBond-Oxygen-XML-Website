15f611f4115f4b666192e5c89bc76432def96291
Jbond0257 <145931890+Jbond0257@users.noreply.github.com>
2023-11-04 15:42:17 -0400

    Add files via upload

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {};
+var linkToParent = {5:-1,8:7,9:7,3:-1,6:-1,11:7,2:-1,7:-1,10:-1,12:7,13:7,4:6,1:-1,14:6};
